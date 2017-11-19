240cc1189f30174434584d65fcea44926f7f04ef
ifaraji <ifaraji@gmail.com>
2017-11-19 21:59:18 +1100

    Implementing - when invalid input is used

--- src/main/java/com/nrp/Calculator.java
+++ src/main/java/com/nrp/Calculator.java
@@ -2,6 +2,8 @@ package com.nrp;
 
 public class Calculator {
     public void readInput(String input){
-
+	String[] tokens = input.split(" ");
+	for (String token : tokens)
+	    Double.parseDouble(token);
     }
 }
