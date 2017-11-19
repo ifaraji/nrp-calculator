2f32bc90c543950832caf0f44013471a19f9bf2c
ifaraji <ifaraji@gmail.com>
2017-11-19 22:23:28 +1100

    Implementing - inclusion of valid operators

--- src/main/java/com/nrp/Calculator.java
+++ src/main/java/com/nrp/Calculator.java
@@ -1,9 +1,20 @@
 package com.nrp;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Calculator {
+    private HashMap<String, String> operators;
+    public Calculator(){
+	operators = new HashMap<String, String>();
+	for (String op : Arrays.asList("+", "-", "*", "/", "sqrt", "undo", "clear"))
+	    operators.put(op, op);
+    }
+	
     public void readInput(String input){
 	String[] tokens = input.split(" ");
 	for (String token : tokens)
-	    Double.parseDouble(token);
+	    if (!operators.containsKey(token))
+		Double.parseDouble(token);
     }
 }
