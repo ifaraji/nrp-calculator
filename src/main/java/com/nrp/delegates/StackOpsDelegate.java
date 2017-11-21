094b867ca90a4f625b908a2578fddb6288859ad8
ifaraji <ifaraji@gmail.com>
2017-11-21 23:51:04 +1100

    StackOpsDelegate - implementing multiple pushes

--- src/main/java/com/nrp/delegates/StackOpsDelegate.java
+++ src/main/java/com/nrp/delegates/StackOpsDelegate.java
@@ -4,7 +4,8 @@ import java.util.Stack;
 
 public class StackOpsDelegate<T> {
     public void pushMultiple(Stack<T> stack, T[] items){
-
+	for (T item : items)
+	    stack.push(item);
     }
     
     public void copyTo(Stack<T> stack1, Stack<T> stack2){
