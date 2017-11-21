99a2878b67df9149f366333e192f10b86c2938b3
ifaraji <ifaraji@gmail.com>
2017-11-22 00:12:00 +1100

    implementing copy stack

--- src/main/java/com/nrp/delegates/StackOpsDelegate.java
+++ src/main/java/com/nrp/delegates/StackOpsDelegate.java
@@ -9,7 +9,8 @@ public class StackOpsDelegate<T> {
     }
     
     public void copyTo(Stack<T> stack1, Stack<T> stack2){
-	
+	for(int i = 0; i < stack1.size(); i++)
+	    stack2.push(stack1.get(i));
     }
     
 }
